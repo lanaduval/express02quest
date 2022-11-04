86b07fee5c9e0c76b4db95a58375c053f74f7893
lana duval <lanaduval09@gmail.com>
2022-11-04 22:55:37 +0100

    Express 02 quest

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.APP_PORT ?? 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
+//welcome
+
+const welcome = (req, res) => {
+  res.send("Welcome to my favourite movie list");
+};
+
+app.get("/", welcome);
+
+// movies
+
+const movieHandlers = require("./movieHandlers");
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+// users
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+
+
